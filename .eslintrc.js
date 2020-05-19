e5970abb3ce5b7f98cacdbb49f26f85d39efe670
HyBeard <Kaptsevich.i@gmail.com>
2020-05-19 18:22:15 +0300

    chore: update babel config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,12 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    'lines-between-class-members': 'off',
+  },
 };
